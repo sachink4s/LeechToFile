b3c2c8c8488be31106f2831abda9f1a583054737
Aid3n_Pearce <61540835+Aid-3n@users.noreply.github.com>
2020-05-22 16:55:06 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def help_message_f(client, message):
     message_id = 99
     # display the /help message
     await message.reply_text(
-        f"Hai,Mate How Are You? Don't Leech Porn"
+        f"Hai,Mate How Are You? Don't Leech Porn",
         quote=True
     )
 
